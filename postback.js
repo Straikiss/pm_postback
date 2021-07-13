015d0dc292d0e5d5b8d8cd91404a32eb8538afff
Straikiss <starko.web@gmail.com>
2021-07-13 14:53:24 +0300

    Update postback.js

--- postback.js
+++ postback.js
@@ -1,16 +1,10 @@
-function PostBack(){
-    var http = new XMLHttpRequest();
-    // var url = "https://postmarket.pro/";
-    // var params = 'lol';
-
-    // http.open("POST", url, true);
-
-    // http.onreadystatechange = function() {
-    //     if(http.readyState == 4 && http.status == 200) {
-    //         alert(http.responseText);
-    //     }
-    //  }
-    // http.send(params);
-    http.open('POST', 'postmarket.pro/cpa/48686', true);
-    http.send('lorem=ipsum&name=binny');
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "https://postmarket.pro/cpa/48686", true);
+  xhttp.send();
 }
